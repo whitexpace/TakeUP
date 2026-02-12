dfb3519ae8fefce8e6d0123c3783d91587b8989d
Glenn Solis <glennpescaderosolis@gmail.com>
2026-02-12 23:40:14 +0800

    format section one left side

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,10 +4,31 @@
       <div class="left">
         <img src="/images/logo.svg" alt="TakeUP Logo" href="" class="left-img">
       </div>
-        
       <div class="right">
         <img src="/images/login-button.svg" alt="Login button" href="">
         <div class="sign-in">Sign in</div>
+      </div>
+    </div>
+
+      <div class="content">
+      <div class="section one">
+        <div class="left">
+          <h1 class="slogan">SHARE WHAT YOU <span class="highlight orange">HAVE</span><br>GET WHAT YOU <span class="highlight blue">NEED</span></h1>
+          <p class="description">Borrow essential gear for free or rent items for your projects within a trusted campus network.</p>
+          <div class="stats">
+            <StatCard />
+            <StatCard />
+            <StatCard />
+          </div>
+        </div>
+        <div class="right">
+          <div>Login component</div>
+          <div>
+            <p>Verified UP Students only</p>  
+            <p>Secure transactions</p>
+            <p>Campus-based meetups</p>
+          </div>
+        </div>
       </div>
     </div>
   </main>
@@ -31,7 +52,7 @@
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 0 40px 0 55px;
+  padding: 0 168px 0 168px;
 }
 
 .left {
@@ -47,6 +68,10 @@
   line-height: 1;
 }
 
+.left-img:hover {
+  cursor: pointer;
+}
+
 .right {
   display: flex;
   align-items: center;
@@ -60,6 +85,10 @@
   display: block;
 }
 
+.right img:hover {
+  cursor: pointer;
+}
+
 .sign-in {
   color: #202124;
   font-size: 16px;
@@ -67,8 +96,40 @@
   line-height: 1;
 }
 
+.sign-in:hover {
+  cursor: pointer;
+}
+
+.content {
+  padding: 0 168px 0 168px;
+  margin-top: 48px;
+}
+
+.section.one {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.section.one .left {
+  max-width: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+.section.one .right {
+  max-width: 50%;
+  justify-content: flex-start;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+
+
 @media (max-width: 768px) {
-  .header {
+  .header, .content {
     padding: 0 16px;
   }
 
