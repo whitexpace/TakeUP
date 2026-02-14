b7fc588f23d260680c4e6ffc841f6fbe72a97022
Glenn Solis <glennpescaderosolis@gmail.com>
2026-02-15 00:41:10 +0800

    feat(home): refactor itemcategories

--- server/api/trpc/[trpc].ts
+++ server/api/trpc/[trpc].ts
@@ -3,7 +3,6 @@ import { appRouter } from '../../trpc/routers'
 import { createContext } from '../../trpc/context'
 
 export default createNuxtApiHandler({
-  endpoint: '/api/trpc',
   router: appRouter,
   createContext,
 })
