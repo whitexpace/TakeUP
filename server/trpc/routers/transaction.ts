629d0553777de558f6793a47cb74cf3500d9c6a1
Glenn Solis <glennpescaderosolis@gmail.com>
2026-03-23 00:44:55 +0800

    fix: align transaction typing across prisma clients

--- server/trpc/routers/transaction.ts
+++ server/trpc/routers/transaction.ts
@@ -29,30 +29,6 @@ const transactionInclude = {
       freeToBorrow: true,
     },
   },
-  borrower: {
-    select: {
-      user: {
-        select: {
-          username: true,
-          firstName: true,
-          middleName: true,
-          lastName: true,
-        },
-      },
-    },
-  },
-  lender: {
-    select: {
-      user: {
-        select: {
-          username: true,
-          firstName: true,
-          middleName: true,
-          lastName: true,
-        },
-      },
-    },
-  },
 } satisfies Prisma.RentalTransactionInclude
 
 const getTransactionThumbnailImage = (item: {
@@ -120,22 +96,6 @@ type TransactionRecord = {
       isPrimary: boolean
       sortOrder: number
     }>
-  } | null
-  borrower: {
-    user: {
-      username: string
-      firstName: string
-      middleName: string | null
-      lastName: string
-    }
-  } | null
-  lender: {
-    user: {
-      username: string
-      firstName: string
-      middleName: string | null
-      lastName: string
-    }
   } | null
 }
 
