8a807106b78b883e674a774b9be9865784c96ca7
Glenn Solis <glennpescaderosolis@gmail.com>
2026-04-18 16:31:15 +0800

    feat(rewards):APP-449 build rewards boost experience

--- app/composables/__tests__/use-my-listings.test.ts
+++ app/composables/__tests__/use-my-listings.test.ts
@@ -39,8 +39,6 @@ const makeItem = (id = ITEM_ID) =>
     lenderUsername: "test-lender",
     lenderFullName: "Test Lender",
     isLiked: false,
-    boostScore: 0,
-    boostExpiresAt: null,
     description: null,
     condition: "GOOD",
     whatItemOffers: null,
