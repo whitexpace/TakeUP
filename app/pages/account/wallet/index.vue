4d4d6c28b4085554c4aa1c1dc54041da6dbe629a
Jed Lordy Legaspo <jed.lordy123@gmail.com>
2026-05-17 12:29:34 +0800

    chore: rebase to dev

--- app/pages/account/wallet/index.vue
+++ app/pages/account/wallet/index.vue
@@ -15,7 +15,6 @@ const {
   topUpPseudo,
   formattedBalance,
   maskedBalance,
-  isInitialLoading,
 } = useWallet()
 </script>
 
--- app/pages/profile/[username].vue
+++ app/pages/profile/[username].vue
@@ -509,7 +509,7 @@ const formatDate = (date: string | Date) => {
 }
 
 .noise-overlay {
-  background-image: url("data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'%3%3Ffilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.65' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E");
+  background-image: url("data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.65' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E");
 }
 
 .hero-content {
